9c769894a5c4acbd9caf727f4ef44ee3c13952e4
abraham sanchez fonseca <abraham_gto@hotmail.com>
2020-06-19 09:19:30 -0500

    Initial Push

---
+++ README.md
@@ -0,0 +1,3 @@
+## TOTP password
+
+Generate
---
+++ src/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+    // public static void GenerateMy() {
+    // Date date = new Date();
+    // System.out.println(date);
+    // String key = "abraham_gto@hotmail.com";
+
+    // long T0 = 0;
+    // long X = 30;
+    // long C = (date.getTime() / 1000);
+    // long T = (C - T0) / X;
+
+    // String steps = Long.toHexString(T).toUpperCase();
+
+    // String code = generateTOTP512(key, steps, "10");
+    // System.out.print(code);
+    // }
+
+}
